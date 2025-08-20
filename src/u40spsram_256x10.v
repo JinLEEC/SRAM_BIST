6c59c199a4aee428860d02bd8efb96de81760cd7
Jin_HARDWARE <ljkrhs3025@gmail.com>
2025-08-20 16:15:32 +0900

    Update u40spsram_256x10.v

--- src/u40spsram_256x10.v
+++ src/u40spsram_256x10.v
@@ -3,17 +3,16 @@ module  u40spsram_256x10 (
     input           clka  , 
     input           ena   , 
     input           wea   , 
-    input  [ 7: 0]  addra , 
-    input  [ 9: 0]  dina  , 
-    output [ 9: 0]  douta  
-
+    input  [7:0]    addra , 
+    input  [9:0]    dina  , 
+    output [9:0]    douta  
   );
 
     U40SP_256X10M4 u_MEMB(
               .Q         (douta  ), 
               .CLK       (clka   ), 
-              .CEN       (~ena    ), 
-              .WEN       (~wea    ), 
+              .CEN       (~ena   ), 
+              .WEN       (~wea   ), 
               .A         (addra  ), 
               .D         (dina   ), 
               .EMA       (3'b000 ), 
